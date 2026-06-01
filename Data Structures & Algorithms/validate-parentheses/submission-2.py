9f61f1309f89e3b73f730fc873d24be366f6755a
MyungJin Ko <gmj1197@gmail.com>
2026-06-01 15:33:27 +0900

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,40 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # day 1 - watch solution
+        stack = []
+        closeToOpen = {")" : "(", "]" : "[", "}" : "{"}
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if not stack else False
+
+        # day 2 - watch before answer
+        stack = []
+        closeToOpen = {")" : "(", "]" : "[", "}" : "{"}
+        
+        for c in s:
+            # is close bracket 
+            if c in closeToOpen:
+                # stack not empty and last item is faire c's open bracket
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                # stack is emtpty or last item is not fair c's open bracket
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        # stack is empty
+        # return True if not stack else False
+
+        if not stack:
+            return True
+        else:
+            return False
