0590caf7136d80db6a47224fc76f975d86c09dab
MyungJin Ko <gmj1197@gmail.com>
2026-06-01 15:18:48 +0900

    Add: buy-and-sell-crypto - submission-10

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-10.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # day 1 - watch solution
+        # # 'l' is buy index, 'r' is sell index
+        # l, r = 0, 1 # Left=buy, right=sell
+        # maxP = 0
+
+        # while r < len(prices):
+        #     #profitable ?
+        #     if prices[l] < prices[r]:
+        #         profit = prices[r] - prices[l]
+        #         print(l, r)
+        #         print(profit)
+        #         maxP = max(maxP, profit)
+        #     else:
+        #         l = r
+        #     r += 1
+
+        # return maxP
+
+        # day 2 - watch before answer
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                # l += 1
+                l = r
+            r += 1
+
+        return maxP
