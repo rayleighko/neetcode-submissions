ce04b46bb57df087f3ca5c1a484fdc2937213250
MyungJin Ko <gmj1197@gmail.com>
2026-06-08 11:00:01 +0900

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 9.4
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "ste"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
